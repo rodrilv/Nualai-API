4b0978fb974c204e6b282f3f181a9a1afc1f0e96
Roy <rott.9954@gmail.com>
2021-11-10 22:52:01 -0600

    2nd

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 process.env.PORT = process.env.PORT || 3000;
 //Entorno de desarrollo 
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || 'prod';
 //Coneccion a base de datos
 process.env.SEED = process.env.SEED || 'firma-super-secreta';
 //Firma webtoken
@@ -9,6 +9,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/usuarios';
 } else {
-    urlDB = 'mongodb://localhost:27017/usuarios';
+    urlDB ="mongodb+srv://root:root@cluster0.ljf5e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 }
 process.env.URLDB = urlDB;

e2a90f1c83dd632dec68f03de28694d9347ad909
rodrilv <rott.9954@gmail.com>
2022-05-11 14:50:48 -0500

    style: changed mongoose model format

--- server/models/usuarios.js
+++ server/models/usuarios.js
@@ -8,79 +8,71 @@ let miembro = new Schema({
     type: String,
     required: [true, "Ingrese un ID para usuario"],
   },
-  datosGenerales: [
-    {
-      nombres: {
-        type: String,
-        required: [true, "Ingrese un Nombre para el miembro"],
-      },
-      edad: {
+  datosGenerales: {
+    nombres: {
+      type: String,
+      required: [true, "Ingrese un Nombre para el miembro"],
+    },
+    edad: {
+      type: Number,
+      required: [true, "Ingrese la Edad para el miembro"],
+    },
+    apellido_paterno: {
+      type: String,
+    },
+    apellido_materno: {
+      type: String,
+    },
+    telefono: {
+      type: String,
+      required: [true, "Ingrese un Telefono para el miembro"],
+    },
+    direccion: {
+      type: String,
+      required: [true, "Ingrese un Telefono para el miembro"],
+    },
+  },
+  datosMedicos: {
+    presionArterial: {
+      sistolica: {
         type: Number,
-        required: [true, "Ingrese la Edad para el miembro"],
       },
-      apellido_paterno: {
-        type: String,
-      },
-      apellido_materno: {
-        type: String,
-      },
-      telefono: {
-        type: String,
-        required: [true, "Ingrese un Telefono para el miembro"],
-      },
-      direccion: {
-        type: String,
-        required: [true, "Ingrese un Telefono para el miembro"],
+      distolica: {
+        type: Number,
       },
     },
-  ],
-  datosMedicos: [
-    {
-      presionArterial: [
-        {
-          sistolica: {
-            type: Number,
-          },
-          distolica: {
-            type: Number,
-          },
-        },
-      ],
-      glucosa: {
-        type: Number,
-      },
-      trigliceridos: {
-        type: Number,
-      },
-      colesterol: [
-        {
-          total: {
-            type: Number,
-          },
-          no_hdl: {
-            type: Number,
-          },
-          ldl: {
-            type: Number,
-          },
-          hdl: {
-            type: Number,
-          },
-        },
-      ],
-      tiroides: [{
-          tsh:{
-              type: Number
-          },
-          t3:{
-              type: Number
-          },
-          t4:{
-              type: Number
-          }
-      }],
+    glucosa: {
+      type: Number,
     },
-  ],
+    trigliceridos: {
+      type: Number,
+    },
+    colesterol: {
+      total: {
+        type: Number,
+      },
+      no_hdl: {
+        type: Number,
+      },
+      ldl: {
+        type: Number,
+      },
+      hdl: {
+        type: Number,
+      },
+    },
+    tiroides: {
+      tsh: {
+        type: Number,
+      },
+      t3: {
+        type: Number,
+      },
+      t4: {
+        type: Number,
+      },
+    },
+  },
 });
 
 miembro.plugin(uniqueValidator, {

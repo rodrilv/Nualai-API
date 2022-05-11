c58acd88fae1fe5c72fa915890b682701cdba725
rodrilv <rott.9954@gmail.com>
2022-05-11 13:57:35 -0500

    feat: added mongoose data model (in)

--- server/models/usuarios.js
+++ server/models/usuarios.js
@@ -1,37 +1,90 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 let Schema = mongoose.Schema;
 
-let usuarios = new Schema({
-    _id:{
+let miembro = new Schema({
+  _id: {
+    type: String,
+    required: [true, "Ingrese un ID para usuario"],
+  },
+  datosGenerales: [
+    {
+      nombres: {
+        type: String,
+        required: [true, "Ingrese un Nombre para el miembro"],
+      },
+      edad: {
         type: Number,
-        required: [true, 'Ingrese un ID para usuario']
-    },
-    Nombre:{
+        required: [true, "Ingrese la Edad para el miembro"],
+      },
+      apellido_paterno: {
         type: String,
-        required: [true, 'Ingrese un Nombre para usuario']
+      },
+      apellido_materno: {
+        type: String,
+      },
+      telefono: {
+        type: String,
+        required: [true, "Ingrese un Telefono para el miembro"],
+      },
+      direccion: {
+        type: String,
+        required: [true, "Ingrese un Telefono para el miembro"],
+      },
     },
-    Edad:{
+  ],
+  datosMedicos: [
+    {
+      presionArterial: [
+        {
+          sistolica: {
+            type: Number,
+          },
+          distolica: {
+            type: Number,
+          },
+        },
+      ],
+      glucosa: {
         type: Number,
-        required: [true, 'Ingrese la Edad para usuario']
+      },
+      trigliceridos: {
+        type: Number,
+      },
+      colesterol: [
+        {
+          total: {
+            type: Number,
+          },
+          no_hdl: {
+            type: Number,
+          },
+          ldl: {
+            type: Number,
+          },
+          hdl: {
+            type: Number,
+          },
+        },
+      ],
+      tiroides: [{
+          tsh:{
+              type: Number
+          },
+          t3:{
+              type: Number
+          },
+          t4:{
+              type: Number
+          }
+      }],
     },
-    Ocupacion:{
-        type: String,
-        required: [true, 'Ingrese la Ocupacion para usuario']
-    },
-    Genero:{
-        type: String,
-        required: [true, 'Ingrese un Genero para usuario']
-    },
-    Pass:{
-        type: String,
-        required: [true, 'Ingresar Password para usuario']
-    }
+  ],
 });
 
-usuarios.plugin(uniqueValidator, {
-    message: "{PATH} Debe ser unico"
+miembro.plugin(uniqueValidator, {
+  message: "{PATH} Debe ser unico",
 });
 
-module.exports = mongoose.model('usuarios', usuarios);
+module.exports = mongoose.model("miembro", miembro);

7ab2ade8d40b29009a15d58b27fbfb25316d3740
rodrilv <rott.9954@gmail.com>
2022-05-16 14:11:18 -0500

    feat: added endpoints to api

--- server/routes/usuarios/miembros.js
+++ server/routes/usuarios/miembros.js
@@ -16,22 +16,22 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/obtener-miembros", (req, res) =>{
-  Miembro.find().exec((err, members) =>{
-    if(err){
+app.get("/obtener-miembros", (req, res) => {
+  Miembro.find().exec((err, members) => {
+    if (err) {
       return res.status(400).json({
         ok: false,
-        err
-      })
-    }else{
-      console.log(members);
+        err,
+      });
+    } else {
+      //console.log(members);
       return res.status(200).json({
         ok: true,
-        members
-      })
+        members,
+      });
     }
-  })
-})
+  });
+});
 
 app.post("/registrar", (req, res) => {
   let i = "NM";
@@ -43,12 +43,77 @@ app.post("/registrar", (req, res) => {
       nombres: req.body.miembro.datosGenerales.nombres,
       apellidos: req.body.miembro.datosGenerales.apellidos,
       edad: req.body.miembro.datosGenerales.edad,
+      genero: req.body.miembro.datosGenerales.genero,
       domicilio: req.body.miembro.datosGenerales.domicilio,
       telefono: req.body.miembro.datosGenerales.telefono,
       correo: req.body.miembro.datosGenerales.correo,
       ocupacion: req.body.miembro.datosGenerales.ocupacion,
       estado_civil: req.body.miembro.datosGenerales.estado_civil,
       actividad_fisica: req.body.miembro.datosGenerales.actividad_fisica,
+    },
+    datosPago: {
+      semanas: [
+        {
+          semana: 1,
+          status: "ADEUDO",
+        },
+        {
+          semana: 2,
+          status: "ADEUDO",
+        },
+        {
+          semana: 3,
+          status: "ADEUDO",
+        },
+        {
+          semana: 4,
+          status: "ADEUDO",
+        },
+        {
+          semana: 5,
+          status: "ADEUDO",
+        },
+        {
+          semana: 6,
+          status: "ADEUDO",
+        },
+        {
+          semana: 7,
+          status: "ADEUDO",
+        },
+        {
+          semana: 8,
+          status: "ADEUDO",
+        },
+        {
+          semana: 9,
+          status: "ADEUDO",
+        },
+        {
+          semana: 10,
+          status: "ADEUDO",
+        },
+        {
+          semana: 11,
+          status: "ADEUDO",
+        },
+        {
+          semana: 12,
+          status: "ADEUDO",
+        },
+        {
+          semana: 13,
+          status: "ADEUDO",
+        },
+        {
+          semana: 14,
+          status: "ADEUDO",
+        },
+        {
+          semana: 15,
+          status: "ADEUDO",
+        },
+      ],
     },
   });
   console.log(miembro);
@@ -67,24 +132,27 @@ app.post("/registrar", (req, res) => {
   });
 });
 
-app.put("/agregar-datos-medicos", (req, res) =>{
+app.put("/agregar-datos-medicos", (req, res) => {});
 
-})
+app.patch("/pagar-semanas", (req, res) => {
+  let body = req.body;
+  console.log(body);
+});
 
-app.delete("/eliminar-miembro/:id", (req, res) =>{
+app.delete("/eliminar-miembro/:id", (req, res) => {
   let id = req.params.id;
-  usuarios.findOneAndDelete({_id: id}, (err) =>{
-    if(err){
+  usuarios.findOneAndDelete({ _id: id }, (err) => {
+    if (err) {
       return res.status(400).json({
         ok: false,
-        err
-      })
-    }else{
+        err,
+      });
+    } else {
       return res.status(200).json({
-        ok: true
-      })
+        ok: true,
+      });
     }
-  })
-})
+  });
+});
 
 module.exports = app;

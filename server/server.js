d67d0f976ceb5266c4c14046676919dbaae8dd2b
rodrilv <rott.9954@gmail.com>
2022-05-16 14:10:20 -0500

    fix: fixed cors patch method

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
     );
     res.setHeader(
         'Access-Control-Allow-Methods',
-        'GET, POST, PUT, DELETE'
+        'GET, POST, PUT, DELETE, PATCH'
     );
     next();
 });
